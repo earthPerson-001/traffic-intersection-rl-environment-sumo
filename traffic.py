5368175ae97f94b98508d794776dc730a486e635
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2022-08-27 12:17:12 +0545

    Corrected yellow lights operation. Some code refactoring

--- traffic.py
+++ traffic.py
@@ -1,17 +1,34 @@
+#! /bin/python
+
+'''
+This script is to test the working of trained model.
+A lot of unnecessary code is here, remove the code for different intersection when not required or 
+make it work for general intersection.
+
+For now all the codes is bundled unordered, separated by if else statements.
+
+Remember to change the directories to your local ones.
+'''
+
+
+
 from __future__ import print_function
 from __future__ import absolute_import
-from genericpath import exists
 
 import os
 import sys
 import optparse
-from time import sleep
+import numpy
 
 from generateRouteFile import generate_routefile
 
-TRAFFIC_INTERSECTION_TYPE="double"
-TOTAL_TIMESTEPS=5000
-GENERATE_CUSTOM_ROUTE=True
+TRAFFIC_INTERSECTION_TYPE="triple"
+TOTAL_TIMESTEPS=50000  # This is the sumo timestep which is somewhat independent of independent of steps in simulation
+                       # rather this decides the number of steps in the simulation
+GENERATE_CUSTOM_ROUTE=False
+
+YELLOW_TIME = 10
+MIN_GREEN_TIME = 30
 
 # checking for sumo_home variable and exiting if it is not found
 if 'SUMO_HOME' in os.environ:
@@ -25,23 +42,26 @@ import sumo.tools.sumolib as sumolib
 from sumo.tools import traci
 
 import gym
-from custom_gym.envs.custom_env_dir import TrafficIntersectionEnvSingleLaneGUI
+from custom_gym.envs.custom_env_dir import TrafficIntersectionEnvTripleLaneGUI
 from stable_baselines3 import PPO
 
+
+# Change the directory to your local ones, absolute paths may work best
 SUMO_DIRECTORY="/home/bishal/Programming/reinforcement-learning-env-gui/sumo-files"
-net_file=SUMO_DIRECTORY + "/small-map-double-lane.net.xml"
-route_file=SUMO_DIRECTORY + "/small-map-double-lane.rou.xml"
-sumocfg_file=SUMO_DIRECTORY + "/small-map-double-lane.sumocfg"
+
+net_file=SUMO_DIRECTORY + "/small-map-{}-lane.net.xml".format(TRAFFIC_INTERSECTION_TYPE)
+route_file=SUMO_DIRECTORY + "/test-small-map-{}-lane.rou.xml".format(TRAFFIC_INTERSECTION_TYPE)
+sumocfg_file=SUMO_DIRECTORY + "/small-map-{}-lane.sumocfg".format(TRAFFIC_INTERSECTION_TYPE)
 
 env = gym.make('TrafficIntersectionEnv{}LaneGUI-v1'.format(TRAFFIC_INTERSECTION_TYPE.capitalize()), sumocfg_file=sumocfg_file, network_file=net_file, route_file=route_file, use_gui=True)
 
 # Here, formatting is done as to create error if wrong model is selected
 # as, there won't be same model trained at exact same time and upto same timesteps
-model = PPO.load("/home/bishal/Programming/reinforcement-learning-env-gui/models/2022-08-19 16:31:36.013366-TrafficIntersection-{}LaneGUI-ppo-145000000".format(TRAFFIC_INTERSECTION_TYPE.capitalize()))
+model = PPO.load("/home/bishal/Programming/reinforcement-learning-env-gui/models/2022-08-26 20:31:22.136701-TrafficIntersection-{}LaneGUI-ppo-300000".format(TRAFFIC_INTERSECTION_TYPE.capitalize()))
 
 def run():
     step = 0
-    
+
     while step < TOTAL_TIMESTEPS:
 
         if step == 0: # setting the initial configuration
@@ -49,79 +69,107 @@ def run():
                 traci.trafficlight.setPhase("J9", 4)
             elif TRAFFIC_INTERSECTION_TYPE == "double":
                 traci.trafficlight.setPhase("J11", 4)
+            elif TRAFFIC_INTERSECTION_TYPE == "triple":
+                traci.trafficlight.setPhase("J1", 4)
 
             traci.simulationStep()
-            step += 1
+            step = traci.simulation.getTime()
             continue
+        current_state = None
+        next_configuration = current_state  # Doing this so that next configuration is defined if none of the conditions is met
+        junction_with_lights = None
 
-        sleep(0.08)
+        if TRAFFIC_INTERSECTION_TYPE == "single":
 
-        # Selection traffic configuration once every 30 timesteps
-        
-        simulation_time = traci.simulation.getTime()
-        if  simulation_time % 30 == 0:
+            junction_with_lights = "J9"
 
-            if TRAFFIC_INTERSECTION_TYPE == "single":
+            current_state = traci.trafficlight.getPhase(junction_with_lights)
 
-                junction_with_lights = "J9"
 
-                current_state = traci.trafficlight.getPhase(junction_with_lights)
+            # starting from left-upper lane, skipping one lane
+            vehicle_count_lane_0 = traci.lane.getLastStepVehicleNumber("-E8_0")
+            vehicle_count_lane_1 = traci.lane.getLastStepVehicleNumber("-E9_0")
+            vehicle_count_lane_2 = traci.lane.getLastStepVehicleNumber("-E10_0")
+            vehicle_count_lane_3 = traci.lane.getLastStepVehicleNumber("E7_0")
 
-                # starting from left-upper lane, skipping one lane
-                vehicle_count_lane_0 = traci.lane.getLastStepVehicleNumber("-E8_0")
-                vehicle_count_lane_1 = traci.lane.getLastStepVehicleNumber("-E9_0")
-                vehicle_count_lane_2 = traci.lane.getLastStepVehicleNumber("-E10_0")
-                vehicle_count_lane_3 = traci.lane.getLastStepVehicleNumber("E7_0")
+            lanes_observation = [vehicle_count_lane_0, vehicle_count_lane_1, vehicle_count_lane_2, vehicle_count_lane_3]
 
-                lanes_observation = [vehicle_count_lane_0, vehicle_count_lane_1, vehicle_count_lane_2, vehicle_count_lane_3]
+            next_configuration, _state = model.predict(lanes_observation, deterministic=True)
 
-                lane_to_have_next_green_light, _state = model.predict(lanes_observation, deterministic=True)
-
-                if lane_to_have_next_green_light == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
-                    traci.trafficlight.setPhase(junction_with_lights, current_state)
-                else:
-                    # Trying to turn on yellow light
-                    # This doesn't work for now, we need to figure out a way to send the next traffic light configuration after turning on yellow light
+            if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
+                traci.trafficlight.setPhase(junction_with_lights, current_state)
+            else:
+                # Trying to turn on yellow light
+                delta_yellow_time = 0
+                while delta_yellow_time < YELLOW_TIME:
                     traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
+                    traci.simulationStep()
+                    delta_yellow_time += 1
 
-                    # changing phase here makes the above phase change obsolete
-                    traci.trafficlight.setPhase(junction_with_lights, lane_to_have_next_green_light * 2)
+        elif TRAFFIC_INTERSECTION_TYPE == "double":
 
-            elif TRAFFIC_INTERSECTION_TYPE == "double":
+            junction_with_lights = "J11"
 
-                junction_with_lights = "J11"
+            current_state = traci.trafficlight.getPhase(junction_with_lights)
 
-                current_state = traci.trafficlight.getPhase(junction_with_lights)
 
-                vehicle_count_lane_0 = traci.lane.getLastStepVehicleNumber("E9_0")
-                vehicle_count_lane_1 = traci.lane.getLastStepVehicleNumber("E9_1")
-                vehicle_count_lane_2 = traci.lane.getLastStepVehicleNumber("E8_0")
-                vehicle_count_lane_3 = traci.lane.getLastStepVehicleNumber("E8_1")
-                vehicle_count_lane_4 = traci.lane.getLastStepVehicleNumber("-E10_0")
-                vehicle_count_lane_5 = traci.lane.getLastStepVehicleNumber("-E10_1")
-                vehicle_count_lane_6 = traci.lane.getLastStepVehicleNumber("-E11_0")
-                vehicle_count_lane_7 = traci.lane.getLastStepVehicleNumber("-E11_1")
+            vehicle_count_lane_0 = traci.lane.getLastStepVehicleNumber("E9_0")
+            vehicle_count_lane_1 = traci.lane.getLastStepVehicleNumber("E9_1")
+            vehicle_count_lane_2 = traci.lane.getLastStepVehicleNumber("E8_0")
+            vehicle_count_lane_3 = traci.lane.getLastStepVehicleNumber("E8_1")
+            vehicle_count_lane_4 = traci.lane.getLastStepVehicleNumber("-E10_0")
+            vehicle_count_lane_5 = traci.lane.getLastStepVehicleNumber("-E10_1")
+            vehicle_count_lane_6 = traci.lane.getLastStepVehicleNumber("-E11_0")
+            vehicle_count_lane_7 = traci.lane.getLastStepVehicleNumber("-E11_1")
 
-                lanes_observation = [vehicle_count_lane_0, vehicle_count_lane_1, vehicle_count_lane_2, vehicle_count_lane_3, vehicle_count_lane_4, vehicle_count_lane_5, vehicle_count_lane_6, vehicle_count_lane_7]
-                next_configuration, _state = model.predict(lanes_observation, deterministic=True)
+            lanes_observation = [vehicle_count_lane_0, vehicle_count_lane_1, vehicle_count_lane_2, vehicle_count_lane_3, vehicle_count_lane_4, vehicle_count_lane_5, vehicle_count_lane_6, vehicle_count_lane_7]
+            next_configuration, _state = model.predict(lanes_observation, deterministic=True)
 
-                if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
-                    traci.trafficlight.setPhase(junction_with_lights, current_state)
-                    
-                    for id in traci.route.getIDList():
-                        print(traci.route.getEdges(id))
-                        print("\n")
-                    
-                else:
-                    # Trying to turn on yellow light
-                    # This doesn't work for now, we need to figure out a way to send the next traffic light configuration after turning on yellow light
+            if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
+                traci.trafficlight.setPhase(junction_with_lights, current_state)
+            else:
+                # Trying to turn on yellow light
+                delta_yellow_time = 0
+                while delta_yellow_time < YELLOW_TIME:
                     traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
+                    traci.simulationStep()
+                    delta_yellow_time += 1
 
-                    # changing phase here makes the above phase change obsolete
-                    traci.trafficlight.setPhase(junction_with_lights, next_configuration * 2)
+        elif TRAFFIC_INTERSECTION_TYPE == "triple":
 
-        traci.simulationStep()
-        step += 1
+            junction_with_lights = "J1"
+
+            current_state = traci.trafficlight.getPhase(junction_with_lights)
+
+            lanes_to_observe = ["E0_0", "E0_1", "E0_2",
+                             "-E1_0", "-E1_1", "-E1_2",
+                             "-E2_0", "-E2_1", "-E2_2",
+                             "-E3_0", "-E3_1", "-E3_2"]
+                
+            lanes_observation = numpy.zeros(lanes_to_observe.__len__())
+            for i, lane in enumerate(lanes_to_observe):
+                lanes_observation[i] = traci.lane.getLastStepVehicleNumber(lane)
+                
+            next_configuration, _state = model.predict(lanes_observation, deterministic=True)
+
+            if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
+                traci.trafficlight.setPhase(junction_with_lights, current_state)
+            else:
+                # Trying to turn on yellow light
+                delta_yellow_time = 0
+                while delta_yellow_time < YELLOW_TIME:
+                    traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
+                    traci.simulationStep()
+                    delta_yellow_time += 1
+
+        # Turning green light for the predefined period of time
+        delta_green_time = 0
+        while delta_green_time < MIN_GREEN_TIME:
+            traci.trafficlight.setPhase(junction_with_lights, next_configuration * 2)
+            traci.simulationStep()
+            delta_green_time += 1
+
+        step = traci.simulation.getTime()
 
 def get_options():
     optParser = optparse.OptionParser()
@@ -133,8 +181,6 @@ def get_options():
 if __name__ == "__main__":
     options = get_options()
 
-    file_separator = os.path.sep
-
     # this script has been called from the command line. It will start sumo as a
     # server, then connect and run
     if options.nogui:
@@ -142,24 +188,14 @@ if __name__ == "__main__":
     else:
         sumoBinary = sumolib.checkBinary('sumo-gui')
 
-    current_directory = os.getcwd()
-    sumo_files_directory = current_directory + file_separator + "sumo-files" + file_separator
-
-    if not exists(sumo_files_directory):
-        sys.exit("Couldn't find sumo files directory, please place it in proper place.")
-    
-
     # Generating custom route file
     if GENERATE_CUSTOM_ROUTE and TRAFFIC_INTERSECTION_TYPE == "double":
         route_file = generate_routefile()
-    else:
-        route_file = "{}small-map-{}-lane.rou.xml".format(sumo_files_directory, TRAFFIC_INTERSECTION_TYPE)
-
+   
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
-    traci.start([sumoBinary, 
-    "-c", "{}small-map-{}-lane.sumocfg".format(sumo_files_directory, TRAFFIC_INTERSECTION_TYPE),
-    "-n", "{}small-map-{}-lane.net.xml".format(sumo_files_directory, TRAFFIC_INTERSECTION_TYPE),
+    traci.start([sumoBinary,
+    "-n", net_file,
     "-r", route_file])
 
     run()
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from custom_gym.envs.custom_env_dir import TrafficIntersectionEnvDoubleLaneGUI
 import gym
 
 TOTAL_TIMESTEPS_FOR_SUMO=500000
-TOTAL_TIMESTEPS_FOR_MODEL=25000 # Switch the traffic light * times
+TOTAL_TIMESTEPS_FOR_MODEL=25000 # call env.step() this many times
 
 startTime = datetime.now()
 
@@ -21,17 +21,16 @@ net_file=SUMO_DIRECTORY + "/small-map-{}-lane.net.xml".format(TRAFFIC_INTERSECTI
 route_file=SUMO_DIRECTORY + "/small-map-{}-lane.rou.xml".format(TRAFFIC_INTERSECTION_TYPE)
 sumocfg_file=SUMO_DIRECTORY + "/small-map-{}-lane.sumocfg".format(TRAFFIC_INTERSECTION_TYPE)
 
-env = gym.make("TrafficIntersectionEnv{}LaneGUI-v1".format(TRAFFIC_INTERSECTION_TYPE.capitalize()), sumocfg_file=sumocfg_file, network_file=net_file, route_file=route_file, use_gui=True, total_timesteps=TOTAL_TIMESTEPS_FOR_SUMO, min_green=30, yellow_time=10)
+env = gym.make("TrafficIntersectionEnv{}LaneGUI-v1".format(TRAFFIC_INTERSECTION_TYPE.capitalize()), sumocfg_file=sumocfg_file, network_file=net_file, route_file=route_file, use_gui=False, total_timesteps=TOTAL_TIMESTEPS_FOR_SUMO, min_green=30, yellow_time=10)
 # env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 env.reset()
 
 # stable_baselines3.common.env_checker.check_env(env, warn=True, skip_render_check=True)
 
-
 modelType = "ppo"
-model = PPO("MlpPolicy", env, device="auto", verbose=1, batch_size=256, n_steps=256, n_epochs=5, tensorboard_log="./logs/{}-trafficintersection-{}-lane-GUI/".format(modelType, TRAFFIC_INTERSECTION_TYPE))
+# model = PPO("MlpPolicy", env, device="auto", verbose=1, batch_size=256, n_steps=256, n_epochs=5, tensorboard_log="./logs/{}-trafficintersection-{}-lane-GUI/".format(modelType, TRAFFIC_INTERSECTION_TYPE))
 
-# model = PPO.load("/home/bishal/Programming/reinforcement-learning-env-gui/models/2022-08-25 21:07:44.963046-TrafficIntersection-{}LaneGUI-ppo-75000".format(TRAFFIC_INTERSECTION_TYPE.capitalize(), modelType), env=env)
+model = PPO.load("/home/bishal/Programming/reinforcement-learning-env-gui/models/2022-08-26 20:31:22.136701-TrafficIntersection-{}LaneGUI-ppo-300000".format(TRAFFIC_INTERSECTION_TYPE.capitalize(), modelType), env=env)
 
 count = 1
 while count < 30:
